a459808f34046284ae1b2cf8fc7470927093a99a
Lucas <lucasgunner3@gmail.com>
2021-12-18 20:43:29 -0300

    Criando entidades

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,46 @@
+﻿namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        protected User()
+        {
+        }
+
+        public User(string name, 
+            string email,
+            string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new();
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            return false;
+        }
+    }
+}
