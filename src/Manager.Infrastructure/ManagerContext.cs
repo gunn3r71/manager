d250581432fcc5c9e7229039beb55cbd76f74db4
Lucas <lucasgunner3@gmail.com>
2021-12-18 23:10:16 -0300

    Criando migrations e configurando contexto

--- src/Manager.Infrastructure/ManagerContext.cs
+++ src/Manager.Infrastructure/ManagerContext.cs
@@ -1,4 +1,5 @@
-﻿using Manager.Domain.Entities;
+﻿using System;
+using Manager.Domain.Entities;
 using Manager.Infrastructure.Mappings;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,15 +7,34 @@ namespace Manager.Infrastructure
 {
     public class ManagerContext : DbContext
     {
+        public ManagerContext()
+        {
+        }
+
         public ManagerContext(DbContextOptions<ManagerContext> options) : base(options)
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            var connectionString = @"Server=localhost;Database=Manager;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString, x =>
+            {
+                x.EnableRetryOnFailure(3, new TimeSpan(0, 0, 0, 30), null);
+                x.CommandTimeout(30);
+                x.MigrationsHistoryTable("Migrations");
+                x.MigrationsAssembly(typeof(ManagerContext).Assembly.FullName);
+            });
         }
     }
 }
---
+++ src/Manager.Infrastructure/Migrations/ManagerMigrations/20211219020936_FirstMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Manager.Infrastructure.Migrations.ManagerMigrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "CHAR(36)", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(90)", maxLength: 90, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Password = table.Column<string>(type: "VARCHAR(8)", maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
