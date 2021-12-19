b375f54f40d93d5078cb81085e378f96ff5e42d8
Lucas <lucasgunner3@gmail.com>
2021-12-18 23:24:30 -0300

    adicionando overload de construtor na domain exception

--- src/Manager.Shared/Exceptions/DomainException.cs
+++ src/Manager.Shared/Exceptions/DomainException.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Manager.Shared.Exceptions
 {
     public class DomainException : Exception
     {
-        public DomainException(string message) : base(message)
+        internal List<string> _errors;
+        public IReadOnlyCollection<string> Errors => _errors;
+
+        public DomainException()
         {
+        }
+
+        public DomainException(string message): base(message)
+        {
+        }
+
+        public DomainException(string message, List<string> errors) : base(message)
+        {
+            _errors = errors;
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public DomainException(string message, List<string> errors, Exception innerException) : base(message, innerException)
+        {
+            _errors = errors;
         }
     }
 }
