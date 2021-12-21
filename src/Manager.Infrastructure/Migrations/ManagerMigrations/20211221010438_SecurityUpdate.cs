3a939b8d02afe4b869b3ae3707a3215ababf3ca6
Lucas <lucasgunner3@gmail.com>
2021-12-20 22:07:07 -0300

    migrations

---
+++ src/Manager.Infrastructure/Migrations/ManagerMigrations/20211221010438_SecurityUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Manager.Infrastructure.Migrations.ManagerMigrations
+{
+    public partial class SecurityUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
